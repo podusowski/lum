6053b3c2e49b03f2ab87ac2ecd22a4d3569891d2
Piotr Podusowski <podusowski@gmail.com>
2022-09-07 12:43:49 +0200

    basic test

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,1 +1,16 @@
 #include <catch2/catch_test_macros.hpp>
+#include <mutex>
+#include <thread>
+
+void increment(int &value, std::mutex &m) {
+  std::unique_lock<std::mutex> guard{m};
+  value++;
+}
+
+TEST_CASE("test") {
+  int value{0};
+  std::mutex m;
+  std::thread t{increment, std::ref(value), std::ref(m)};
+  REQUIRE(1 == value);
+  t.join();
+}
