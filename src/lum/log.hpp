0ea1bd25e3e6936b6a1ab8e5ce821fd08663359b
Piotr Podusowski <podusowski@gmail.com>
2022-09-09 17:26:52 +0200

    LUM_TRACE env var

--- src/lum/log.hpp
+++ src/lum/log.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <mutex>
@@ -8,15 +9,24 @@
 namespace lum {
 
 struct trace {
-  trace() { std::cerr << "thread:" << std::this_thread::get_id() << ": "; }
-  ~trace() { std::cerr << std::endl; }
+  trace() : enabled(std::getenv("LUM_TRACE") != nullptr) {
+    if (enabled)
+      std::cerr << "thread:" << std::this_thread::get_id() << ": ";
+  }
+
+  ~trace() {
+    if (enabled)
+      std::cerr << std::endl;
+  }
 
   template <class T> trace &operator<<(T &&t) {
-    std::cerr << t;
+    if (enabled)
+      std::cerr << t;
     return *this;
   }
 
 private:
+  bool enabled{true};
   inline static std::mutex _mutex;
   std::unique_lock<std::mutex> _lock{_mutex};
 };
